15b0d46c8bbf53e9b0be6ac1c5ba3e0653127560
ankushnr <1nt23is028.ankush@nmit.ac.in>
2026-03-08 00:12:38 +0530

    add lambda function code to backend folder

---
+++ backend/lambda_function.py
@@ -0,0 +1,13 @@
+cd ..
+git add .
+git commit -m "add lambda function code"
+git push origin backend
+```
+
+This way your code is properly saved on GitHub too! 👍
+
+---
+
+After this, tell Manvith to start the frontend work and share him this API URL:
+```
+https://svzju8smoa.execute-api.ap-south-1.amazonaws.com/prod/upload
